494c3afb65a236fc33c97e22a6e9861f61839aef
FauzanAr <faurinta12345@gmail.com>
2019-12-12 19:38:02 +0700

    Finish setting up index configuration to connect to firebase db

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,20 @@
 import * as functions from 'firebase-functions';
 
 import * as admin from 'firebase-admin';
-import * as firebaseHelper from 'firebase-functions-helper';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 
+admin.initializeApp(functions.config().firebase);
+
+const db = admin.firestore();
+
+const app = express();
+const main = express();
+
+const postsCollection = 'posts';
+
+main.use('/api', app);
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({ extended: false }));
+
+export const webApi = functions.https.onRequest(main);
